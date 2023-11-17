458a8983afb859300cd6c8d0eef4d16846f583ee
quocgiau <quocgiau61s@gmail.com>
2023-11-17 16:02:55 +0700

    first commit

--- ss3_thao_tac_voi_csdl/bai_tap/bai2/them_du_lieu.sql
+++ ss3_thao_tac_voi_csdl/bai_tap/bai2/them_du_lieu.sql
@@ -1,22 +1,22 @@
 INSERT INTO customer (`name`,age)
-VALUE ('Minh Quan', 10),
+VALUES ('Minh Quan', 10),
 ('Ngoc Oanh', 20),
 ('Hong Ha', 50);
 
 INSERT INTO `order` (`date`, c_id)
-VALUE ('2006-03-21', 1),
+VALUES ('2006-03-21', 1),
 ('2006-03-23', 2),
 ('2006-03-16', 1);
 
 INSERT INTO product (`name`, price)
-VALUE ('May Giat', 3),
+VALUES ('May Giat', 3),
 ('Tu Lanh', 5),
 ('Dieu Hoa', 7),
 ('Quat', 1),
 ('Bep Dien', 2);
 
 INSERT INTO order_detail (o_id, p_id, qty)
-VALUE (1, 1, 3),
+VALUES (1, 1, 3),
 (1, 3, 7),
 (1, 4, 2),
 (2, 1, 1),
